f9085d231c7a5624dfb699b62e5439c428f98232
Clismandev <morumbi841@gmail.com>
2024-10-15 15:48:44 -0300

    polimorfismo commit

---
+++ Polimorfismo/src/principal/Cachorro.java
@@ -0,0 +1,9 @@
+package principal;
+
+public class Cachorro extends Mamifero{
+	@Override
+	public void emitirSom() {
+		System.out.println("au au au au");
+	}
+
+}
---
+++ Polimorfismo/src/principal/Cobra.java
@@ -0,0 +1,5 @@
+package principal;
+
+public class Cobra extends Reptil {
+
+}
